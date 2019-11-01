c3509ab4f6b1b5af343dcb2a77c37678e2c9c6b9
lpgoulart <leonardo@roffor.com.br>
2019-11-01 08:27:49 -0300

    create dashboard service

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DataService } from "./data.service";
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
@@ -8,7 +8,8 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [DataService]
     })
     .compileComponents();
   }));
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-
+import { DataService } from "./data.service";
 
 
 @NgModule({
@@ -11,6 +11,9 @@ import { DashboardComponent } from './dashboard.component';
   ],
   exports: [
     DashboardComponent
+  ],
+  providers: [
+    DataService
   ]
 })
 export class DashboardModule { }
